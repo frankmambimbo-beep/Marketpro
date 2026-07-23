3de770457b64b085777bcf9c90db4a9d51516624
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-23 23:38:47 +0200

    Create lead_model.dart

---
+++ lib/models/lead_model.dart
@@ -0,0 +1,21 @@
+class LeadModel {
+
+  String name;
+  String company;
+  String phone;
+  String status;
+
+
+  LeadModel({
+
+    required this.name,
+
+    required this.company,
+
+    required this.phone,
+
+    required this.status,
+
+  });
+
+}
