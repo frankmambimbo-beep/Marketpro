7d772a9c9ae4eedd90d7e14d40bda103b1a88d47
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-23 23:19:35 +0200

    Create project_model.dart

---
+++ lib/models/project_model.dart
@@ -0,0 +1,24 @@
+class ProjectModel {
+
+  String title;
+
+  String client;
+
+  String status;
+
+  double budget;
+
+
+  ProjectModel({
+
+    required this.title,
+
+    required this.client,
+
+    required this.status,
+
+    required this.budget,
+
+  });
+
+}
