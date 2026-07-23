a7a938108706693f810b183e69ce6cbcf615ec37
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-23 23:20:05 +0200

    Add ServiceModel class with properties

---
+++ lib/models/service_model.dart
@@ -0,0 +1,20 @@
+class ServiceModel {
+
+  String name;
+
+  String description;
+
+  double price;
+
+
+  ServiceModel({
+
+    required this.name,
+
+    required this.description,
+
+    required this.price,
+
+  });
+
+}
