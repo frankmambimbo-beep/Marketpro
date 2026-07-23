2de90fe5bf364efdd2a42b67f32c02eae9a8e7c4
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-23 23:41:59 +0200

    Create AppointmentModel class for appointment data

---
+++ lib/models/appointment_model.dart
@@ -0,0 +1,24 @@
+class AppointmentModel {
+
+  String title;
+
+  String client;
+
+  String date;
+
+  String time;
+
+
+  AppointmentModel({
+
+    required this.title,
+
+    required this.client,
+
+    required this.date,
+
+    required this.time,
+
+  });
+
+}
