b856026d0b1ed1572f87f7013ecfb5a63a9e57e4
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-23 23:21:53 +0200

    Add AppConstants class with application constants

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,20 @@
+class AppConstants {
+
+
+  static const String appName =
+      "MarketPro";
+
+
+  static const String version =
+      "1.0.0";
+
+
+  static const String supportEmail =
+      "support@marketpro.com";
+
+
+  static const String currency =
+      "\$";
+
+
+}
