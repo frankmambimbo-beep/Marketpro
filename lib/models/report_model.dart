b20f39f45924bd7470167773e478e3807b032aba
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-27 20:08:17 +0200

    Create report_model.dart

---
+++ lib/models/report_model.dart
@@ -0,0 +1,21 @@
+class ReportModel {
+
+
+  String title;
+
+  String description;
+
+  String date;
+
+
+  ReportModel({
+
+    required this.title,
+
+    required this.description,
+
+    required this.date,
+
+  });
+
+}
