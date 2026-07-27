83b55c28a633a7eaffea5d5deaaad062f833ee39
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-27 19:50:11 +0200

    Create content_model.dart

---
+++ lib/models/content_model.dart
@@ -0,0 +1,24 @@
+class ContentModel {
+
+  String title;
+
+  String platform;
+
+  String date;
+
+  String status;
+
+
+  ContentModel({
+
+    required this.title,
+
+    required this.platform,
+
+    required this.date,
+
+    required this.status,
+
+  });
+
+}
