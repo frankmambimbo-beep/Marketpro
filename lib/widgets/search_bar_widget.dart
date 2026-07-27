2378faf244afdf63ce347b187df5c964533fb55f
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-27 20:03:22 +0200

    Add SearchBarWidget for search functionality

---
+++ lib/widgets/search_bar_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+
+class SearchBarWidget extends StatelessWidget {
+
+  final Function(String) onChanged;
+
+
+  const SearchBarWidget({
+
+    super.key,
+
+    required this.onChanged,
+
+  });
+
+
+  @override
+  Widget build(BuildContext context){
+
+    return TextField(
+
+      onChanged:onChanged,
+
+
+      decoration:
+      InputDecoration(
+
+        hintText:
+        "Search...",
+
+
+        prefixIcon:
+        const Icon(
+          Icons.search,
+        ),
+
+
+        border:
+        OutlineInputBorder(
+
+          borderRadius:
+          BorderRadius.circular(20),
+
+        ),
+
+      ),
+    );
+  }
+}
