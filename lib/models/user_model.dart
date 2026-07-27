322255cbc2bdca75d3a8ea46a19fc99f292a6843
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-27 19:57:13 +0200

    Create user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,20 @@
+class UserModel {
+
+  String name;
+
+  String email;
+
+  String company;
+
+
+  UserModel({
+
+    required this.name,
+
+    required this.email,
+
+    required this.company,
+
+  });
+
+}
