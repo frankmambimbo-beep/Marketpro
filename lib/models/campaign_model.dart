514649bc3164714342eb4af5319323f753ec8c0e
frankmambimbo-beep <frankmambimbo@gmail.com>
2026-07-27 19:48:21 +0200

    Create campaign_model.dart

---
+++ lib/models/campaign_model.dart
@@ -0,0 +1,24 @@
+class CampaignModel {
+
+  String name;
+
+  String platform;
+
+  String status;
+
+  double budget;
+
+
+  CampaignModel({
+
+    required this.name,
+
+    required this.platform,
+
+    required this.status,
+
+    required this.budget,
+
+  });
+
+}
